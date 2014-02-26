cf551b7c1ceda1901f1f4f84dcf8ea550f7f1c03
Anthony Chin <wei-wing.chin@mail.mcgill.ca>
2014-02-26 02:50:13 -0500

    complete functionality

--- calculateRscore.js
+++ calculateRscore.js
@@ -1,42 +1,38 @@
-
-// standard r score avg ranges between 18-40
-var myGrade = document.getElementById('my-grade').value;
-var classAverage = document.getElementById('class-average').value;
-var highschoolClassAverage = document.getElementById('highschool-class-average').value;
-var standardDeviation = document.getElementById('standard-deviation').value;
-
 // (Z Score + Group Strength + 5) x 5
 // group strength is (average cegep class -75)/14
 // Z score = (Your grade - class average)/standard deviation
+var myGrade;
+var classAverage;
+var highschoolClassAverage;
+var standardDeviation;
+
 function calculateRScore () {
-	var calculateRScore = (calculateZScore + calculateGroupStrength + 5)*5;
-	return calculateRScore;
+	myGrade = parseInt(document.getElementById('my-grade').value);
+	classAverage = parseInt(document.getElementById('class-average').value);
+	highschoolClassAverage = parseInt(document.getElementById('highschool-class-average').value);
+	standardDeviation = parseFloat(document.getElementById('standard-deviation').value);
+	var rScore = (calculateZScore() + calculateGroupStrength() + 5)*5;
+	document.getElementById("result").innerHTML = rScore;
 }
 
-// grade below 50 are not included
 // Z score cannot exceed 3 or -3
 function calculateZScore () {
-	var zScore = (myGrade - classAverage)/standardDeviation
+	var zScore = (myGrade - classAverage)/standardDeviation;
 	if (myGrade == 100 && zScore < 2.0) {
 		return 2.0;
 	} 
 
-	if (zScore) > 3.0 {
+	if (zScore > 3.0) {
 		return 3.0;
-	} else if (zScore) < 3.0 {
+	} else if (zScore < -3.0) {
 		return -3.0;
 	}
 	return zScore;
 }
 
 function calculateGroupStrength () {
-	var groupStrength = (highschoolClassAverage - 75)/14
+	var groupStrength = (highschoolClassAverage - 75)/14;
 	return groupStrength
 }
-
-$("#response").click(function () {
-	alert("hello");
-	response.innerHTML = calculateRScore();
-});
 
 //http://www.slc.qc.ca/sites/slc/files/documents/the_r_score_presentation.pdf
